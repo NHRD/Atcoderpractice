9c2ef59c29d543d52361aaf127a6c74278341a91
Naohisa Harada <naohisa.harada@gmail.com>
2020-07-17 18:03:09 +0900

    aa

--- Atcoder_problems/ABC002/atcoder_habatsu.py
+++ Atcoder_problems/ABC002/atcoder_habatsu.py
@@ -1,1 +1,52 @@
 n, m = map(int, input().split())
+
+if m == 0:
+    print("1")
+    exit()
+
+rels = []
+combs =[]
+
+for i in range(m):
+    rel = list(map(int, input().split()))
+    rels.append(rel)
+    rel = []
+
+rels = sorted(rels)
+#print(rels)
+
+
+#print(mb)
+pos = []
+res = 0
+
+for k in range(n):
+    mb = 2 ** (k + 1) - 1
+    poss = []
+    for i in range(mb):
+        bins = bin(i)[2:]
+        #print(bins)
+        sums = 0
+        for j in range(len(bins)):
+            sums = sums + int(bins[j])
+            #print(sums)
+        if sums == 2:
+            bins = "0" * (m - len(bins)) + bins
+            #print(bins)
+            for i in range(len(bins)):
+                if bins[i] == "1":
+                    pos.append(i+1)
+            poss.append(pos)
+            pos = []
+        else:
+            continue
+        #print(poss)
+    for l in range(len(poss)):
+        if poss != rels:
+            #print(poss[l])
+            print(len(poss))
+            exit()
+        else:
+            continue
+        
+#print(poss)
