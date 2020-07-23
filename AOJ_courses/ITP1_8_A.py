37d4bad5c716e7e4ca55f729c6c8abd074a82db8
Naohisa Harada <naohisa.harada@gmail.com>
2020-07-23 15:54:50 +0900

    aa

--- AOJ_courses/ITP1_8_A.py
+++ AOJ_courses/ITP1_8_A.py
@@ -1,26 +1,4 @@
 sent = input()
 
-for i in sent:
-    if i.isalnum():
-        if i.isupper():
-            if i == sent[len(sent) - 1]:
-                print(i.lower()) 
-            else:
-                 print(i.lower(), end="")  
-
-        elif i.islower():
-            if i == sent[len(sent) - 1]:
-                print(i.upper())
-            else:
-                print(i.upper(), end="")
-        else:
-            if i == sent[len(sent) - 1]:
-                print(i)
-            else:
-                print(i, end="")
-    else:
-        if i == sent[len(sent) - 1]:
-            print(i)
-        else:
-            print(i, end="")
-    
+s = sent.swapcase()
+print(s)
