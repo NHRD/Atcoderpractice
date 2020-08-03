4ec9c91d25032e44160311f36dfcffefd94a2ec4
NHRD <naohisa.harada@gmail.com>
2020-08-03 23:58:47 +0900

    aa

--- Beginner_contest174/C.py
+++ Beginner_contest174/C.py
@@ -1,18 +1,18 @@
-k = input()
-ik = int(k)
-
-#sevens = [7, 77, 777, 7777, 77777, 777777, 7777777, 77777777, 777777777, 7777777777, 77777777777]
-
-
-if k[0] not in ["1", "3" ,"7", "9"]:
+k = int(input())
+if k % 7 == 0:
+    L = 9 * k / 7
+else:
+    L = 9 * k
+ 
+if L % 2 == 0 or L % 5 == 0:
     print(-1)
-    exit()
 
-for i in range(len(k), len(k) + 1000000):
-    sev = "7" * i
-    ise = int(sev)
-    if ise % int(k) == 0:
-        print(len(sev))
+m = 10
+
+for i in range(k):
+    m = m % L
+    if m == 1:
+        print(i + 1)
         exit()
-
-print(-1)
+    else:
+        m = m * 10
