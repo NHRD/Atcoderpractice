1ec9445c8cfe0aa1ad3b416ec683536662033e8f
Naohisa Harada <naohisa.harada@gmail.com>
2020-09-01 17:01:57 +0900

    aa

--- ABC_177/C.py
+++ ABC_177/C.py
@@ -1,17 +1,11 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-comb = len(a) * (len(a) - 1) / 2
-comb = int(comb)
+su = sum(a)
+sus = 0
 
-sum = 0
+for i in a:
+    su = su - i
+    sus = sus + i * su
 
-for i in range(comb):
-    #print(a)
-    for i in range(1, len(a)):
-        #print(a[i])
-        sum = sum + a[0] * a[i]
-    a = a[1:]
-    #print(a)
-
-print(sum % (10**9 + 7))
+print(sus % (10**9 + 7))
