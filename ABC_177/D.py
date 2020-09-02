e221d5147c19ea6a894b100b29233a6e82d594f9
Naohisa Harada <naohisa.harada@gmail.com>
2020-09-02 14:53:43 +0900

    aa

--- ABC_177/D.py
+++ ABC_177/D.py
@@ -2,13 +2,30 @@ from statistics import mode
 
 n, m = map(int, input().split())
 
-rels = [i for i in range(n + 1)]
+rels = [i for i in range(n)]
+inf = []
+count = [0] * n
 
 for i in range(m):
     rel = list(map(int, input().split()))
     rel = sorted(rel)
-    a = rel[0]
-    b = rel[1]
+    inf.append(rel)
+
+inf = sorted(inf)
+
+for rel in inf:
+    a = rel[0] - 1
+    b = rel[1] - 1
     rels[b] = rels[a]
-    
+
+"""
+for rel in rels:
+    count[rel - 1] = count[rel - 1] + 1
+
+res = rels.count(mode(rels))
+
+while res < sum(count) - res:
+    res = res + 1
+    """
+
 print(rels.count(mode(rels)))
